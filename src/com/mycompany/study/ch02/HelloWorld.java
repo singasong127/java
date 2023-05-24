bb831ad0a5e8243a538a23ec959ec0325009c98f
singasong127 <songjy0127@naver.com>
2023-05-24 15:36:39 +0900

    f.create java

---
+++ src/com/mycompany/study/ch02/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.mycompany.study.ch02;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("이름 : 송재윤");
+		System.out.println("나이 : 24");
+		System.out.println("성별 : 남");
+		System.out.println("주소 : 경기도 김포시 통진읍");
+		
+	}
+
+}
